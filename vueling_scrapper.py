c07803925b405064de386544949b11e605bb787c
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-10-22 01:04:50 +0200

    Option commented

--- vueling_scrapper.py
+++ vueling_scrapper.py
@@ -57,7 +57,7 @@ class VuelingScrapper:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--disable-gpu")
