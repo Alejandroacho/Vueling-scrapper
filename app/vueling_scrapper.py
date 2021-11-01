b878cbd3db914a5b53484b76be2d02082b827bcc
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-11-01 21:53:13 +0100

    Removed unnecesary things

--- app/vueling_scrapper.py
+++ app/vueling_scrapper.py
@@ -8,10 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from webdriver_manager.chrome import ChromeDriverManager
 
 DEPLOYED = True
 LAST_SLIDES = 0
@@ -79,11 +77,6 @@ class VuelingScrapper:
     def get_chrome_options(self):
         chrome_options = Options()
         chrome_options.add_argument("--no-sandbox")
-        # chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument('--disable-dev-shm-usage')
         return chrome_options
 
     def allow_cookies(self, driver):
